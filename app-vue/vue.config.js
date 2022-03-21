205f732422b9c7265cc34fa95cb6d64231ba64b6
welkin <welkin105380@163.com>
2022-03-21 23:10:21 +0800

    fix: 添加主页面配置

--- app-vue/vue.config.js
+++ app-vue/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require("@vue/cli-service");
+const { name } = require('./package');
+
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  configureWebpack: {
+    output: {
+      library: `${name}-[name]`,
+      libraryTarget: 'umd', // 把微应用打包成 umd 库格式
+      chunkLoadingGlobal: `webpackJsonp_${name}`,
+    },
+  },
+})
